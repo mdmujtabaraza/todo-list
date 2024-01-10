5fcc8f895a5fd93102e40ef9552084c5fe2e41f6
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2024-01-10 22:31:59 +0530

    16. Styled TodoList Component

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -68,8 +68,11 @@
 									checked={completed}
 								/>{title}
 							</label>
-							<button aria-label="Remove todo: {title}" on:click={() => handleRemoveTodo(id)}
-								><Icon icon="line-md:remove" /></button
+							<button
+								class="remove-todo-button"
+								aria-label="Remove todo: {title}"
+								on:click={() => handleRemoveTodo(id)}
+								><Icon icon="line-md:close-circle-twotone" color="#bd1414" /></button
 							>
 						</li>
 					{/each}
@@ -79,7 +82,7 @@
 	</div>
 	<form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
 		<input bind:this={input} bind:value={inputText} placeholder="New todo" />
-		<Button type="submit" disabled={!inputText}>Add</Button>
+		<Button class="add-todo-button" type="submit" disabled={!inputText}>Add</Button>
 	</form>
 </div>
 
@@ -87,8 +90,76 @@
 	input {
 		color: #000;
 	}
-	.todo-list {
-		max-height: 150px;
-		overflow: auto;
+	.todo-list-wrapper {
+		background-color: #424242;
+		border: 1px solid #4b4b4b;
+		.no-items-text {
+			// margin: 0;
+			padding: 15px;
+			text-align: center;
+		}
+		.todo-list {
+			max-height: 200px;
+			overflow: auto;
+			ul {
+				// margin: 0;
+				// list-style: none;
+				padding: 10px;
+				li {
+					margin-bottom: 5px;
+					display: flex;
+					align-items: center;
+					background-color: #303030;
+					border-radius: 5px;
+					padding: 10px;
+					position: relative;
+					label {
+						cursor: pointer;
+						font-size: 18px;
+						display: flex;
+						align-items: baseline;
+						padding-right: 20px;
+						input[type='checkbox'] {
+							margin: 0 10px 0 0;
+							cursor: pointer;
+						}
+					}
+					&.completed > label {
+						opacity: 0.5;
+						text-decoration: line-through;
+					}
+					.remove-todo-button {
+						padding: 5px;
+						position: absolute;
+						right: 10px;
+						display: none;
+					}
+					&:hover {
+						.remove-todo-button {
+							display: block;
+						}
+					}
+				}
+			}
+		}
+		.add-todo-form {
+			padding: 15px;
+			background-color: #303030;
+			display: flex;
+			border-top: 1px solid #4b4b4b;
+			flex-wrap: wrap;
+			// :global(.add-todo-button) {
+			// 	background-color: aqua;
+			// }
+			input {
+				flex: 1;
+				background-color: #424242;
+				border: 1px solid #4b4b4b;
+				padding: 10px;
+				color: #fff;
+				border-radius: 5px;
+				margin-right: 10px;
+			}
+		}
 	}
 </style>
